0c9b6cd065d9cdfaba31b32549b12941f73673d7
Chad Jablonski <chad@dinocore.net>
2018-07-21 22:03:28 -0700

    Create use-engine cask

---
+++ Casks/use-engine.rb
@@ -0,0 +1,10 @@
+cask 'use-engine' do
+  version '2.0.1'
+  sha256 'b21337da50a6811091363d4da631052cb33964e859fbe3f82a8922c64aeb5dfd'
+
+  url 'https://repository.use-together.com/stable/use-engine/macos/latest_version/use-engine-2.0.1-macos.dmg'
+  name 'USE Engine'
+  homepage 'https://www.use-together.com/'
+
+  app 'USE Engine.app'
+end
